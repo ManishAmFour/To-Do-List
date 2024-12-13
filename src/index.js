5fa189cc57601cf0ceeee47f2964d320e243a110
Manish Tewatia <ManishTewatia3020@gmail.com>
2024-12-13 16:18:10 +0530

    made the basic todo list class which will create the list when called

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-import { formatDistance, subDays } from "date-fns";
+class ToDoList{
 
+ constructor(title,description,dueDate,priority,notes,checklist){
+
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+
+
+    }
+
+    ConstructToDO(){
+
+        return `${this.title} + ${this.description} + ${this.dueDate}`
+
+
+    }
+
+
+
+
+}
+
+const firstTask = new ToDoList (`Hobbit`,`I like to watch this movie`,`20th december`);
+
+//console.log(firstTask)
